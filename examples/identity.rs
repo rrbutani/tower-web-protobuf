8e2db21c738ae544261d7320fd6af01ec8a18308
Rahul Butani <rr.butani@gmail.com>
2019-04-11 17:57:22 -0500

    add an example / continue working on the README

--- examples/identity.rs
+++ examples/identity.rs
@@ -1,14 +1,3 @@
-# Tower Web Protobuf
-
-Protobuf middleware and friends for [tower-web](https://github.com/carllerche/tower-web).
-
-[![Build Status](https://travis-ci.com/rrbutani/tower-web-protobuf.svg?branch=master)](https://travis-ci.com/rrbutani/tower-web-protobuf)
-[![Docs](https://img.shields.io/badge/docs-v0.1.0-blue.svg)](https://rrbutani.github.io/tower-web-protobuf/tower-web-protobuf/)
-[![Coverage Status](https://coveralls.io/repos/github/rrbutani/tower-web-protobuf/badge.svg?branch=master)](https://coveralls.io/github/rrbutani/tower-web-protobuf?branch=master)
-[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
-
-## [A quick example](examples/identity.rs)
-```rust
 extern crate prost;
 #[macro_use]
 extern crate tower_web;
@@ -48,15 +37,9 @@ pub fn main() {
 
     ServiceBuilder::new()
         .resource(HelloWorld)
+        .middleware(ProtobufMiddleware::new(true, true))
         // (true, true) permits JSON decoding (requests) and encoding
         // (responses)
-        .middleware(ProtobufMiddleware::new(true, true))
         .run(&addr)
         .unwrap();
 }
-```
-## Usage
-
-## How does it work?
-
-<TODO: replace docs with docs.rs link, add license from crates.io, add crates.io link)
